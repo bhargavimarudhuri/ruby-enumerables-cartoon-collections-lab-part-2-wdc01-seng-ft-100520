b81beae4438f11879bf29b1393cd604ec98dd763
Naga Bhargavi Marudhuri <bhargavimarudhuri@gmail.com>
2020-10-02 03:43:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
-  array.map do |element|
+  array.each do |element|
     element * element
   end
 end
@@ -9,7 +9,7 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
-  array.map do |element|
+  array.each do |element|
     element.capitalize + "!"
   end
 end
